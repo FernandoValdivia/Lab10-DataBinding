3cb1649c424614b8c69d98120aaceea66865798e
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2021-05-11 21:15:11 -0500

    First Commit

---
+++ DataBinding/DataBinding/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnBMDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            btnLVDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            btnPKDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
